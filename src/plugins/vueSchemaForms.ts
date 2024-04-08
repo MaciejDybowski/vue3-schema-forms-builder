45c1c8358fa5b1ea013a722e61723f4b953d97ee
Maciej <maciej.dybowski@tecna.pl>
2024-04-08 22:02:40 +0200

    builder - dev

--- src/plugins/vueSchemaForms.ts
+++
@@ -1,7 +0,0 @@
-import {createVueSchemaForms} from "vue3-schema-forms";
-import "vue3-schema-forms/dist/style.css"
-
-const vueSchemaForms = createVueSchemaForms({
-  formUpdateLogger: true
-})
-export default vueSchemaForms
