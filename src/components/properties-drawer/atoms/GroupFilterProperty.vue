a88c4804f3a48bc28b4d93a3a199827c0606d375
Maciej <maciej.dybowski@tecna.pl>
2024-07-27 13:46:02 +0200

    GroupFilterProperty.vue in UserInputProperties.vue

---
+++ src/components/properties-drawer/atoms/GroupFilterProperty.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-list-item>
+    <v-text-field
+      class="pt-2"
+      :label="t('groupFilterProperty')"
+      persistent-hint
+      :hint='t("filterHint")'
+      v-model="modelValue"
+      v-bind="style.inputStyle.value"
+    />
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import {useI18n} from "vue-i18n";
+import {useStyle} from "@/main";
+
+const modelValue = defineModel({})
+const style = useStyle()
+
+const {t} = useI18n()
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<i18n lang="json">
+{
+  "en": {
+    "groupFilterProperty": "Group",
+    "filterHint": "Group names separated by coma"
+  },
+  "pl": {
+    "groupFilterProperty": "Grupa",
+    "filterHint": "Nazwa grupy oddzielana przecinkami"
+  }
+}
+</i18n>
--- src/components/properties-drawer/controls/UserInputProperties.vue
+++ src/components/properties-drawer/controls/UserInputProperties.vue
@@ -6,6 +6,15 @@
   <required-property v-model="model.required"/>
   <read-only-property v-model="model.layout.props.readOnly"/>
   <if-property v-model="model.layout.if"/>
+
+
+  <v-list-item density="compact">
+    <v-divider/>
+  </v-list-item>
+  <v-list-item density="compact">
+    <span class="text-subtitle-1">{{ t("filterTitle") }}</span>
+  </v-list-item>
+  <group-filter-property v-model="filter.group"/>
 </template>
 
 <script setup lang="ts">
@@ -19,8 +28,10 @@ import FillRowProperty from "@/components/properties-drawer/atoms/FillRowPropert
 import RequiredProperty from "@/components/properties-drawer/atoms/RequiredProperty.vue";
 import ReadOnlyProperty from "@/components/properties-drawer/atoms/ReadOnlyProperty.vue";
 import IfProperty from "@/components/properties-drawer/atoms/IfProperty.vue";
-import OffsetProperty from "@/components/properties-drawer/atoms/OffsetProperty.vue";
+import GroupFilterProperty from "@/components/properties-drawer/atoms/GroupFilterProperty.vue";
+import {useI18n} from "vue-i18n";
 
+const {t} = useI18n()
 const useBuilderStateStore = useBuilderState()
 const model = computed({
   get() {
@@ -31,9 +42,37 @@ const model = computed({
   }
 })
 
+
+const filter = computed({
+  get() {
+    if (!model.value.filter) {
+      return {
+        group: null
+      }
+    } else {
+      return model.value.filter
+    }
+  },
+  set(val) {
+    console.debug(val)
+    useBuilderStateStore.setKeyInConfiguredField("filter", val)
+  }
+})
+
 </script>
 
 
 <style scoped lang="scss">
 
 </style>
+
+<i18n lang="json">
+{
+  "en": {
+    "filterTitle": "Filters"
+  },
+  "pl": {
+    "filterTitle": "Filtry"
+  }
+}
+</i18n>
