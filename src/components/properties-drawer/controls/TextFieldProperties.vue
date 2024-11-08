77abd278d7a77525a00b7e2e7e77a5cf345f4754
Maciej Dybowski <maciej.dybowski@tecna.pl>
2024-11-08 19:58:59 +0100

    [0.0.90] - uncomment code

--- src/components/properties-drawer/controls/TextFieldProperties.vue
+++ src/components/properties-drawer/controls/TextFieldProperties.vue
@@ -1,7 +1,7 @@
 <template>
   <key-property v-model="model.key"/>
   <label-property v-model="model.label"/>
-  <!--  <is-number-property v-model="model.type"/>
+    <is-number-property v-model="model.type"/>
     <col-property v-model="model.layout.cols"/>
     <offset-property v-model="model.layout.offset"/>
     <fill-row-property v-model="model.layout.fillRow"/>
@@ -34,7 +34,7 @@
     <checkbox-general
       :label="t('persistentHint')"
       v-model="model.layout.props['persistent-hint']"
-    />-->
+    />
 
   <v-list-item>
     <v-list-item-title>{{ t('validations') }}</v-list-item-title>
@@ -62,6 +62,16 @@ import {useI18n} from "vue-i18n";
 import NumberGeneral from "@/components/properties-drawer/atoms/NumberGeneral.vue";
 import SelectGeneral from "@/components/properties-drawer/atoms/SelectGeneral.vue";
 import EventConfiguration from "@/components/properties-drawer/atoms/EventConfiguration.vue";
+import FillRowProperty from "@/components/properties-drawer/atoms/FillRowProperty.vue";
+import ColProperty from "@/components/properties-drawer/atoms/ColProperty.vue";
+import IsNumberProperty from "@/components/properties-drawer/atoms/isNumberProperty.vue";
+import OffsetProperty from "@/components/properties-drawer/atoms/OffsetProperty.vue";
+import RequiredProperty from "@/components/properties-drawer/atoms/RequiredProperty.vue";
+import ReadOnlyProperty from "@/components/properties-drawer/atoms/ReadOnlyProperty.vue";
+import IfProperty from "@/components/properties-drawer/atoms/IfProperty.vue";
+import SwitchGeneral from "@/components/properties-drawer/atoms/SwitchGeneral.vue";
+import TextfieldGeneral from "@/components/properties-drawer/atoms/TextfieldGeneral.vue";
+import CheckboxGeneral from "@/components/properties-drawer/atoms/CheckboxGeneral.vue";
 
 const useBuilderStateStore = useBuilderState()
 const model = computed({
