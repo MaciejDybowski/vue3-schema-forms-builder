f966533ccf92650618dfc7a79fbda4041b683836
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-03-31 10:52:47 +0200

    Imports

--- src/components/VueSchemaFormsBuilder.vue
+++ src/components/VueSchemaFormsBuilder.vue
@@ -35,10 +35,10 @@ import {computed, FunctionPlugin, getCurrentInstance} from "vue";
 import {FormSchema} from "@/models/FormSchema";
 
 
-import {createVueSchemaForms} from "../../../aurea-forms";
-import "../../../aurea-forms/dist/style.css"
-//import {createVueSchemaForms} from "vue3-schema-forms";
-//import "vue3-schema-forms/dist/style.css"
+//import {createVueSchemaForms} from "../../../aurea-forms";
+//import "../../../aurea-forms/dist/style.css"
+import {createVueSchemaForms} from "vue3-schema-forms";
+import "vue3-schema-forms/dist/style.css"
 
 const instance = getCurrentInstance();
 
