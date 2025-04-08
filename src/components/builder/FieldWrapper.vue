37fd6e9a1453507e35bdb834970a7c2af1263a02
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-04-08 10:30:15 +0200

    fillRow calc alg.

--- src/components/builder/FieldWrapper.vue
+++ src/components/builder/FieldWrapper.vue
@@ -21,7 +21,7 @@
 // @ts-nocheck
 import FieldWrapperItem from "./FieldWrapperItem.vue";
 import {useColSizeMapper} from "@/composables/useColSizeMapper";
-import {computed, nextTick, onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 
 const props = defineProps<{
   element: any,
@@ -36,45 +36,52 @@ const width = computed(() => {
   const offset = isOffsetExist ? (props.element.layout?.offset as number) : 0;
 
   if (fillRow.value) {
-    return `min-width: ${(offset + cols) / 12 * 100}%; margin-right: ${(remainingSpace.value / 12) * 100}%!important;`
+    calculate()
+    return `min-width: ${(offset + cols) / 12 * 100}%; margin-right: ${(fillToEnd.value / 12) * 100}%!important;`
   }
   return `min-width: ${(offset + cols) / 12 * 100}%;`
 })
 
-const isLastInRow = ref(false);
-const remainingSpace = ref(0);
 const colRef = ref<HTMLElement | null>(null);
+const fillToEnd = ref(0)
 
-onMounted(async () => {
-  await nextTick(() => {
-    if (colRef.value) {
-      //@ts-ignore
-      const el = colRef.value.$el as HTMLElement;
-      let parentRow = el.closest(".v-row");
-      if (!parentRow) return;
+function calculate() {
+  const parentRow = colRef.value.$el.closest('.v-row'); // szukamy <v-row> po DOM
+  if (!parentRow) {
+    console.warn('Nie znaleziono v-row');
+    return;
+  }
 
-      let siblings = Array.from(parentRow.children) as HTMLElement[];
-      let totalCols = 0;
+  const cols = Array.from(parentRow.querySelectorAll('.v-col'));
 
-      for (let i = 0; i < siblings.length; i++) {
-        let sibling = siblings[i];
-        let match = sibling.className.match(/col-(\d+)/);
-        let offsetMatch = sibling.className.match(/offset-(\d+)/);
-        let siblingCols = match ? parseInt(match[1]) : 0;
-        let siblingOffset = offsetMatch ? parseInt(offsetMatch[1]) : 0;
+  const index = cols.findIndex(c => c.contains(colRef.value?.$el));
+  //console.log('Mój index:', index);
 
-        if (totalCols + siblingCols + siblingOffset > 12) {
-          totalCols = 0;
-        }
-        totalCols += siblingCols + siblingOffset;
-        if (sibling === el) {
-          isLastInRow.value = totalCols === 12;
-          remainingSpace.value = 12 - totalCols;
-          break;
-        }
-      }
-    }
+  let position = 0;
+  const structure = cols.map(col => {
+    const match = col.getAttribute('class')?.match(/col-(\d+)/);
+    const colSize = match ? parseInt(match[1], 10) : 12;
+    const start = position;
+    const end = position + colSize - 1;
+    position += colSize;
+    return {col, start, end};
   });
+
+  const myStructure = structure[index];
+  //console.log('Moje start-end:', myStructure);
+
+  const hasBefore = index > 0;
+  const hasAfter = structure.length > index + 1;
+  fillToEnd.value = 11 - myStructure.end;
+
+  /*console.log('Mam coś przed sobą?', hasBefore);
+  console.log('Mam coś za sobą?', hasAfter);
+  console.log('Ile wolnego miejsca do końca wiersza:', fillToEnd.value);*/
+}
+
+
+onMounted(() => {
+  calculate()
 });
 </script>
 
--- src/stories/builder.stories.ts
+++ src/stories/builder.stories.ts
@@ -18,29 +18,7 @@ type Story = StoryObj<typeof meta>;
 export const FormBuilderEmpty: Story = {
   args: {
     workspaceId: "bm",
-    modelValue: {
-      "type": "object",
-      "properties": {
-        "static-content-719": {
-          "content": {"$ref": "#/i18n/~$locale~/staticContent719"},
-          "layout": {"component": "static-content", "tag": "h1"}
-        },
-        "test": {
-          "properties": {
-            "poqwert": {
-              "content": {"$ref": "#/i18n/~$locale~/testPoqwert"},
-              "layout": {"component": "static-content", "tag": "p"}
-            }
-          }
-        },
-        "text-field-010": {"label": {"$ref": "#/i18n/~$locale~/textField010"}, "layout": {"component": "text-field"}}
-      },
-      "i18n": {
-        "pl": {"staticContent719": "qwewqe", "testPoqwert": "qwe", "textField010": "qwewqewq"},
-        "en": {"staticContent719": "teqwe", "testPoqwert": "qwewq", "textField010": "qwewqeqewq"},
-        "de": {"staticContent719": "qwewqe", "testPoqwert": "qwewqe", "textField010": "qwewqeeqweqw"}
-      }
-    }
+    modelValue: {"type":"object","properties":{}}
   }
 }
 
