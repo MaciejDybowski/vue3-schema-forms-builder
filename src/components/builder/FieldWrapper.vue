f6cf616efa013a83361a0d4bb0cb51aa136f13d7
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-04-09 19:04:26 +0200

    fillRow

--- src/components/builder/FieldWrapper.vue
+++ src/components/builder/FieldWrapper.vue
@@ -1,25 +1,24 @@
 <template>
-  <div
-    :style="calcWidth(element)"
-    class="mb-3"
+  <v-col
+    :cols="cols"
   >
     <v-hover>
       <template v-slot:default="{ isHovering, props }">
         <field-wrapper-item
           :element="element"
-          :is-hovering="isHovering"
+          :is-hovering="isHovering as boolean"
           v-bind="props"
         />
       </template>
     </v-hover>
-  </div>
+  </v-col>
+  <div v-if="fillRow" class="fill-row"/>
 </template>
 
 <script lang="ts" setup>
-// @ts-nocheck
 import FieldWrapperItem from "./FieldWrapperItem.vue";
 import {useColSizeMapper} from "@/composables/useColSizeMapper";
-import {SchemaField} from "vue3-schema-forms";
+import {computed} from "vue";
 
 const props = defineProps<{
   element: any,
@@ -27,21 +26,19 @@ const props = defineProps<{
 
 const {colSize} = useColSizeMapper()
 
-function calcWidth(element: SchemaField) {
-  const isOffsetExist = !!element.layout?.offset;
-  const offset = isOffsetExist ? (element.layout?.offset as number) : 0;
-  const cols: number = colSize(element);
-  const fillRow = !!element.layout?.fillRow && cols < 12
-
-  if (fillRow) {
-    const paddingRight = (12 - (offset + cols)) / 12 * 100
-    return `min-width:100%; padding-right: ${paddingRight}%!important;`
-  }
-
-  return `min-width: ${(offset + cols) / 12 * 100}%;`
-}
+const fillRow = computed(() => {
+  return !!props.element.layout?.fillRow && colSize(props.element) < 12
+})
+const cols = computed(() => {
+  return colSize(props.element)
+})
 </script>
 
 <style lang="scss" scoped>
+
+.fill-row {
+  flex-basis: 100%;
+  height: 0;
+}
 
 </style>
