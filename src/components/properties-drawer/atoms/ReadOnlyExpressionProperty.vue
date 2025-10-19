dd310c583a0e736eebd4d443eb9068fe2396410e
Maciej Dybowski <maciej.dybowski@tecna.pl>
2025-10-19 13:41:57 +0200

    LogicPanel

--- src/components/properties-drawer/atoms/ReadOnlyExpressionProperty.vue
+++ src/components/properties-drawer/atoms/ReadOnlyExpressionProperty.vue
@@ -1,14 +1,35 @@
 <template>
-
   <div>
     <text-property-wrapper
       v-model="model"
-      :label="t('readonlyExpression')"
-      append-inner-icon="mdi-cog"
-      @click:append-inner="openAdvancedDialog"
       :grow-enabled="false"
+      :label="t('readonlyExpression')"
       :rows="3"
-    />
+    >
+      <template #append-inner>
+        <div class="append-inner-column">
+          <v-tooltip
+            location="left"
+            width="280"
+          >
+            <template #activator="{ props }">
+              <v-icon v-bind="props">
+                mdi-information-outline
+              </v-icon>
+            </template>
+            <span>{{ t('readonlyExpressionInfo') }}</span>
+          </v-tooltip>
+          <v-icon
+            class="clickable"
+            @click="openAdvancedDialog"
+          >
+            mdi-cog
+          </v-icon>
+
+        </div>
+      </template>
+    </text-property-wrapper>
+
     <tcn-au-dialog
       v-if="showAdvancedDialog"
       v-model="showAdvancedDialog"
@@ -26,7 +47,6 @@
         </v-card-title>
       </template>
       <v-card-text class="px-0">
-
         <tcn-code-editor
           v-model="modelInDialog"
           :codemirrorOptions="{}"
@@ -36,7 +56,6 @@
       </v-card-text>
     </tcn-au-dialog>
   </div>
-
 </template>
 
 <script lang="ts" setup>
@@ -45,29 +64,39 @@ import TextPropertyWrapper from "@/components/properties-drawer/atoms/TextProper
 import {ref} from "vue";
 import {useStyle} from "@/main";
 
-const {t} = useI18n()
-const model = defineModel()
-const style = useStyle()
+const {t} = useI18n();
+const model = defineModel();
+const style = useStyle();
 
-const showAdvancedDialog = ref(false)
-const modelInDialog = ref<any>(null)
+const showAdvancedDialog = ref(false);
+const modelInDialog = ref<any>(null);
 
 function openAdvancedDialog() {
-  modelInDialog.value = model.value
-  showAdvancedDialog.value = true
+  modelInDialog.value = model.value;
+  showAdvancedDialog.value = true;
 }
+
 function cancel() {
-  showAdvancedDialog.value = false
+  showAdvancedDialog.value = false;
 }
 
 function save() {
-  model.value = modelInDialog.value
-  showAdvancedDialog.value = false
+  model.value = modelInDialog.value;
+  showAdvancedDialog.value = false;
 }
 </script>
 
 <style lang="scss" scoped>
+.append-inner-column {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px; /* odstęp między ikonami */
+}
 
+.append-inner-column .clickable {
+  cursor: pointer;
+}
 </style>
 
 <i18n lang="json">
@@ -75,11 +104,13 @@ function save() {
   "en": {
     "advancedConfiguration": "Advanced configuration",
     "readonlyExpression": "Readonly expression",
+    "readonlyExpressionInfo": "You can define a JSONATA expression for the readonly property that controls whether this field is editable. Open the advanced editor for more complex expressions.",
     "save": "Save"
   },
   "pl": {
     "advancedConfiguration": "Konfiguracja zaawansowana",
     "readonlyExpression": "Wyrażenie readonly",
+    "readonlyExpressionInfo": "Możesz zdefiniować wyrażenie JSONATA dla właściwości readonly, które kontroluje, czy to pole jest edytowalne. Otwórz zaawansowany edytor dla bardziej złożonych wyrażeń.",
     "save": "Zapisz"
   }
 }
--- src/components/properties-drawer/panels/LogicPanel.vue
+++ src/components/properties-drawer/panels/LogicPanel.vue
@@ -5,8 +5,12 @@
     value="logic"
   >
     <if-property v-model="model.layout.if"/>
-    <read-only-property v-model="model.layout.props.readonly"/>
     <visible-property v-model="model.layout.hide"/>
+    <read-only-property v-model="model.layout.props.readonly"/>
+    <read-only-expression-property
+      :model-value="model.layout.props.readonly"
+      @update:model-value="updateExpressionReadonly"
+    />
   </expansion-panel>
 </template>
 
@@ -16,12 +20,21 @@ import {useI18n} from "vue-i18n";
 import IfProperty from "@/components/properties-drawer/atoms/IfProperty.vue";
 import ReadOnlyProperty from "@/components/properties-drawer/atoms/ReadOnlyProperty.vue";
 import VisibleProperty from "@/components/properties-drawer/atoms/VisibleProperty.vue";
+import ReadOnlyExpressionProperty from "@/components/properties-drawer/atoms/ReadOnlyExpressionProperty.vue";
 
 const {t} = useI18n();
 const {active} = defineProps<{
   active: boolean;
 }>()
 const model = defineModel<any>()
+
+function updateExpressionReadonly(val: string) {
+  if (!val) {
+    model.value.layout.props['readonly'] = false
+  } else {
+    model.value.layout.props['readonly'] = val
+  }
+}
 </script>
 
 <style lang="scss" scoped>
